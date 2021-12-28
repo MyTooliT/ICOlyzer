28f851aa66792dfba56ff947a3bd45b6ce3f054e
IFT\burgstaller <burgstaller@ift.at>
2021-12-28 08:58:58 +0100

    analyzer: implemented 2+3 axis support and 1axis y or z support

--- ICOanalyzer.py
+++ ICOanalyzer.py
@@ -18,6 +18,8 @@ class FileInformation():
         self.packets = 0
         self.datapoints = 0
         self.out_of_range = 0
+        self.out_of_range2 = 0
+        self.out_of_range3 = 0
 
 
 def get_arguments(element):
@@ -37,18 +39,16 @@ def get_arguments(element):
     parser.add_argument('-d', '--details', action='store_true',
                         help='Show more Information about Paketloss')
     args = parser.parse_args()
-
-    check_values = False
     details_on = False
 
     if args.min is not None:
         test_value_min = float(args.min)
-        check_values = True
+        print('MINIMUM CHANGED')
     else:
         test_value_min = -1
     if args.value is not None:
         test_value_max = float(args.value)
-        check_values = True
+        print('MAXIMUM CHANGED')
     else:
         test_value_max = 1
     if args.input is not None:
@@ -59,27 +59,24 @@ def get_arguments(element):
         print('DETAILS ENABLED')
     else:
         details_on = False
-    if check_values is True:
-        print('VALUE-CHECK ENABLED')
-    return element, test_value_min, test_value_max, check_values, details_on
+    return element, test_value_min, test_value_max, details_on
 
 
 def main():
     """
     Main function of the ICOanalyzer.
     """
-    check_values = False
     details_on = False
     test_value_max = 1
     test_value_min = -1
     element = FileInformation('log.hdf5')
 
-    element, test_value_min, test_value_max, check_values, details_on = get_arguments(
+    element, test_value_min, test_value_max, details_on = get_arguments(
         element)
 
     print('Input file is: ' + element.filename)
-    print("Starting ckeck of " + element.filename)
     data = pd.read_hdf(element.filename, key="acceleration")
+
     first_counter = data["counter"][0]
     for datapoint in data["counter"]:
         if (datapoint == first_counter+1) or (datapoint == 0 and first_counter == 255):
@@ -97,22 +94,177 @@ def main():
             first_counter = datapoint
             element.packets = element.packets + lost_packets + 1
             element.packet_loss = element.packet_loss + lost_packets
-        while (element.datapoints) < (len(data["x"])):
+    nr_of_axis = 0
+    x = None
+    y = None
+    z = None
+    try:
+        x = data["x"]
+        nr_of_axis = nr_of_axis + 1
+    except:
+        pass
+    try:
+        y = data["y"]
+        nr_of_axis = nr_of_axis + 1
+    except:
+        pass
+    try:
+        z = data["z"]
+        nr_of_axis = nr_of_axis + 1
+    except:
+        pass
+    if nr_of_axis == 1:
+        while (element.datapoints) < (len(data["timestamp"])):
+            if x is not None:
+                acc_data = data["x"][element.datapoints]
+            elif y is not None:
+                acc_data = data["y"][element.datapoints]
+            elif z is not None:
+                acc_data = data["z"][element.datapoints]
+            element.datapoints = element.datapoints + 1
+            if (acc_data > test_value_max) or (acc_data < test_value_min):
+                if x is not None:
+                    element.out_of_range = element.out_of_range + 1
+                elif y is not None:
+                    element.out_of_range2 = element.out_of_range2 + 1
+                elif z is not None:
+                    element.out_of_range3 = element.out_of_range3 + 1
+    elif nr_of_axis == 2:
+        while (element.datapoints) < (len(data["timestamp"])):
+            if x is None:
+                acc_data2 = data["y"][element.datapoints]
+                acc_data3 = data["z"][element.datapoints]
+                element.datapoints = element.datapoints + 1
+                if (acc_data2 > test_value_max) or (acc_data2 < test_value_min):
+                    element.out_of_range2 = element.out_of_range2 + 1
+                if (acc_data3 > test_value_max) or (acc_data3 < test_value_min):
+                    element.out_of_range3 = element.out_of_range3 + 1
+            elif y is None:
+                acc_data = data["x"][element.datapoints]
+                acc_data3 = data["z"][element.datapoints]
+                element.datapoints = element.datapoints + 1
+                if (acc_data > test_value_max) or (acc_data < test_value_min):
+                    element.out_of_range = element.out_of_range + 1
+                if (acc_data3 > test_value_max) or (acc_data3 < test_value_min):
+                    element.out_of_range3 = element.out_of_range3 + 1
+            elif z is None:
+                acc_data = data["x"][element.datapoints]
+                acc_data2 = data["y"][element.datapoints]
+                element.datapoints = element.datapoints + 1
+                if (acc_data > test_value_max) or (acc_data < test_value_min):
+                    element.out_of_range = element.out_of_range + 1
+                if (acc_data2 > test_value_max) or (acc_data2 < test_value_min):
+                    element.out_of_range2 = element.out_of_range2 + 1
+    elif nr_of_axis == 3:
+        while (element.datapoints) < (len(data["timestamp"])):
             acc_data = data["x"][element.datapoints]
+            acc_data2 = data["y"][element.datapoints]
+            acc_data3 = data["z"][element.datapoints]
             element.datapoints = element.datapoints + 1
             if (acc_data > test_value_max) or (acc_data < test_value_min):
                 element.out_of_range = element.out_of_range + 1
-    print("Finished ckeck of " + element.filename)
-    print("PACKETLOSS: " +
-          str(round((element.packet_loss / element.packets)*100, 2)) + "%")
-    if check_values is True:
+            if (acc_data2 > test_value_max) or (acc_data2 < test_value_min):
+                element.out_of_range2 = element.out_of_range2 + 1
+            if (acc_data3 > test_value_max) or (acc_data3 < test_value_min):
+                element.out_of_range3 = element.out_of_range3 + 1
+    else:
+        print("Error: Number of axis found is not valid")
+    print("PACKETLOSS:")
+    print(str(round((element.packet_loss / element.packets)*100, 2)) + "%")
+    if nr_of_axis == 1:
         percent_overflow = (element.out_of_range /
                             element.datapoints)*100
         percent_overflow = round(percent_overflow, 2)
-        print("DATAPOINTS: " + str(element.out_of_range) +
+        if x is not None:
+            print("DATAPOINTS:")
+            print(str(element.out_of_range) +
+                  " Samples were over " + str(test_value_max) +
+                  "g\u2080 or below " + str(test_value_min) +
+                  "g\u2080 ("+str(percent_overflow)+"%)")
+        elif y is not None:
+            print("DATAPOINTS:")
+            print(str(element.out_of_range2) +
+                  " Samples were over " + str(test_value_max) +
+                  "g\u2080 or below " + str(test_value_min) +
+                  "g\u2080 ("+str(percent_overflow)+"%)")
+        elif z is not None:
+            print("DATAPOINTS:")
+            print(str(element.out_of_range3) +
+                  " Samples were over " + str(test_value_max) +
+                  "g\u2080 or below " + str(test_value_min) +
+                  "g\u2080 ("+str(percent_overflow)+"%)")
+    elif nr_of_axis == 2:
+        if x is None:
+            percent_overflow2 = (element.out_of_range2 /
+                                 element.datapoints)*100
+            percent_overflow2 = round(percent_overflow2, 2)
+            percent_overflow3 = (element.out_of_range3 /
+                                 element.datapoints)*100
+            percent_overflow3 = round(percent_overflow3, 2)
+            print("DATAPOINTS:")
+            print("Y-AXIS: " + str(element.out_of_range2) +
+                  " Samples were over " + str(test_value_max) +
+                  "g\u2080 or below " + str(test_value_min) +
+                  "g\u2080 ("+str(percent_overflow2)+"%)")
+            print("Z-AXIS: " + str(element.out_of_range3) +
+                  " Samples were over " + str(test_value_max) +
+                  "g\u2080 or below " + str(test_value_min) +
+                  "g\u2080 ("+str(percent_overflow3)+"%)")
+        elif y is None:
+            percent_overflow = (element.out_of_range /
+                                element.datapoints)*100
+            percent_overflow = round(percent_overflow, 2)
+            percent_overflow3 = (element.out_of_range3 /
+                                 element.datapoints)*100
+            percent_overflow3 = round(percent_overflow3, 2)
+            print("DATAPOINTS:")
+            print("X-AXIS: " + str(element.out_of_range) +
+                  " Samples were over " + str(test_value_max) +
+                  "g\u2080 or below " + str(test_value_min) +
+                  "g\u2080 ("+str(percent_overflow)+"%)")
+            print("Z-AXIS: " + str(element.out_of_range3) +
+                  " Samples were over " + str(test_value_max) +
+                  "g\u2080 or below " + str(test_value_min) +
+                  "g\u2080 ("+str(percent_overflow3)+"%)")
+        elif z is None:
+            percent_overflow = (element.out_of_range /
+                                element.datapoints)*100
+            percent_overflow = round(percent_overflow, 2)
+            percent_overflow2 = (element.out_of_range2 /
+                                 element.datapoints)*100
+            percent_overflow2 = round(percent_overflow2, 2)
+            print("DATAPOINTS:")
+            print("X-AXIS: " + str(element.out_of_range) +
+                  " Samples were over " + str(test_value_max) +
+                  "g\u2080 or below " + str(test_value_min) +
+                  "g\u2080 ("+str(percent_overflow)+"%)")
+            print("Y-AXIS: " + str(element.out_of_range2) +
+                  " Samples were over " + str(test_value_max) +
+                  "g\u2080 or below " + str(test_value_min) +
+                  "g\u2080 ("+str(percent_overflow2)+"%)")
+    elif nr_of_axis == 3:
+        percent_overflow = (element.out_of_range /
+                            element.datapoints)*100
+        percent_overflow = round(percent_overflow, 2)
+        percent_overflow2 = (element.out_of_range2 /
+                             element.datapoints)*100
+        percent_overflow2 = round(percent_overflow2, 2)
+        percent_overflow3 = (element.out_of_range3 /
+                             element.datapoints)*100
+        percent_overflow3 = round(percent_overflow3, 2)
+        print("DATAPOINTS:")
+        print("X-AXIS: " + str(element.out_of_range) +
               " Samples were over " + str(test_value_max) +
               "g\u2080 or below " + str(test_value_min) +
               "g\u2080 ("+str(percent_overflow)+"%)")
+        print("Y-AXIS: " + str(element.out_of_range2) +
+              " Samples were over " + str(test_value_max) +
+              "g\u2080 or below " + str(test_value_min) +
+              "g\u2080 ("+str(percent_overflow2)+"%)")
+        print("Z-AXIS: " + str(element.out_of_range3) +
+              " Samples were over " + str(test_value_max) +
+              "g\u2080 or below " + str(test_value_min) +
+              "g\u2080 ("+str(percent_overflow3)+"%)")
 
 
 if __name__ == "__main__":
