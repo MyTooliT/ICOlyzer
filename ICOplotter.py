0043ae159fd90cb95ac80e59ecc41a9598b5e3f6
René Schwaiger <sanssecours@me.com>
2022-01-10 13:40:57 +0100

    Plotter: Remove duplicated code

--- ICOplotter.py
+++ ICOplotter.py
@@ -63,39 +63,28 @@ def main():
         axis = ('x'
                 if x_data is not None else 'y' if y_data is not None else 'z')
         std_dev = stats.loc['std', [axis]]
-        snr = 20 * np.log10(std_dev / (np.power(2, 16) - 1))
-        print("SNR of this file is : {:.2f} dB and {:.2f} dB @ {:.2f} kHz".
-              format(min(snr), max(snr), f_sample / 1000))
     elif nr_of_axis == 3:
         std_dev = stats.loc['std', ['x', 'y', 'z']]
         print("Avg  X: %d Y: %d Z: %d" %
               (stats.loc['mean', ['x']], stats.loc['mean', ['y']],
                stats.loc['mean', ['z']]))
-        snr = 20 * np.log10(std_dev / (np.power(2, 16) - 1))
-        print("SNR of this file is : {:.2f} dB and {:.2f} dB @ {:.2f} kHz".
-              format(min(snr), max(snr), f_sample / 1000))
     elif nr_of_axis == 2:
         if x_data is None:
             std_dev = stats.loc['std', ['y', 'z']]
             print("Avg  Y: %d Z: %d " %
                   (stats.loc['mean', ['y']], stats.loc['mean', ['z']]))
-            snr = 20 * np.log10(std_dev / (np.power(2, 16) - 1))
-            print("SNR of this file is : {:.2f} dB and {:.2f} dB @ {:.2f} kHz".
-                  format(min(snr), max(snr), f_sample / 1000))
         elif y_data is None:
             std_dev = stats.loc['std', ['x', 'z']]
             print("Avg  X: %d Z: %d " %
                   (stats.loc['mean', ['x']], stats.loc['mean', ['z']]))
-            snr = 20 * np.log10(std_dev / (np.power(2, 16) - 1))
-            print("SNR of this file is : {:.2f} dB and {:.2f} dB @ {:.2f} kHz".
-                  format(min(snr), max(snr), f_sample / 1000))
         elif z_data is None:
             std_dev = stats.loc['std', ['x', 'y']]
             print("Avg  X: %d Y: %d " %
                   (stats.loc['mean', ['x']], stats.loc['mean', ['y']]))
-            snr = 20 * np.log10(std_dev / (np.power(2, 16) - 1))
-            print("SNR of this file is : {:.2f} dB and {:.2f} dB @ {:.2f} kHz".
-                  format(min(snr), max(snr), f_sample / 1000))
+
+    snr = 20 * np.log10(std_dev / (np.power(2, 16) - 1))
+    print("SNR of this file is : {:.2f} dB and {:.2f} dB @ {:.2f} kHz".format(
+        min(snr), max(snr), f_sample / 1000))
 
     fig, axs = plt.subplots(2, 1, figsize=(20, 10))
     plt.subplot(211)
